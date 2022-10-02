c47cac8f77717a81c1a43ef49f544192f1e12b55
itsTodFod <96081283+itsTodFod@users.noreply.github.com>
2022-10-03 00:42:25 +0530

    Base App

--- app/src/main/java/com/example/facefam/MainActivity.kt
+++ app/src/main/java/com/example/facefam/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.facefam.ui.theme.FaceFamTheme
+import com.example.facefam.ui.theme.VideoScreen
+
+const val APP_ID = "b642039422a840efa287ca818afc14a8"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +29,34 @@ class MainActivity : ComponentActivity() {
             FaceFamTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.padding(16.dp),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "room_screen"
+                    ) {
+                        composable(route = "room_screen") {
+                            RoomScreen(onNavigate = navController::navigate)
+                        }
+                        composable(
+                            route = "video_screen/{roomName}",
+                            arguments = listOf(
+                                navArgument(name = "roomName") {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) {
+                            val roomName = it.arguments?.getString("roomName") ?: return@composable
+                            VideoScreen(
+                                roomName = roomName,
+                                onNavigateUp = navController::navigateUp
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun FaceFamPreview() {
-    FaceFamTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/facefam/ui/theme/VideoViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.facefam.ui.theme
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class VideoViewModel: ViewModel() {
+
+    private val _hasAudioPermission = mutableStateOf(false)
+    val hasAudioPermission: State<Boolean> = _hasAudioPermission
+
+    private val _hasCameraPermission = mutableStateOf<Boolean>(false)
+    val hasCameraPermission: State<Boolean> = _hasCameraPermission
+
+    fun onPermissionsResult(
+        acceptedAudioPermission: Boolean,
+        acceptedCameraPermission: Boolean
+    ) {
+        _hasAudioPermission.value = acceptedAudioPermission
+        _hasCameraPermission.value = acceptedCameraPermission
+    }
+}
